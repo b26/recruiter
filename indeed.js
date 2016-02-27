8a217bdcab9bb364341009d12dc362a58297d232
Bashir Osman <bashir@b26.co>
2016-02-26 20:31:14 -0700

    adding facebook login capability

--- indeed.js
+++ indeed.js
@@ -1,9 +1,50 @@
 var utils = require('utils');
 var _ = require('underscore');
+var fb = require('fb.json');
 
 EASILY = "Easily apply";
+EASILY2 = "Easily apply to this job";
 URI = "http://www.indeed.com";
 var jobs = [];
+
+casper.test.begin('goes to indeed.com and signs in', 1, function(test) {
+    casper.start(URI, function() {
+                if (this.exists('#userOptionsLabel')) {
+                    this.click('#userOptionsLabel');
+                    this.waitUntilVisible('a.FBConnectButton', function () {
+                        this.click('a.FBConnectButton');
+                        this.waitForPopup(/facebook/, function () {
+                            this.echo(casper.popups.length);
+                            test.assertEquals(casper.popups.length, 1);
+                            this.withPopup(/facebook/, function () {
+                                this.echo(this.getCurrentUrl());
+                                /*
+                                    id: email, pass, loginbutton
+                                */
+                                this.waitUntilVisible('#email', function () {
+                                    this.sendKeys('input#email', fb.email);
+                                    this.sendKeys('input#pass', fb.password);
+                                    this.click('#loginbutton');
+                                });
+
+                            });
+                            /* userOptionsLabel */
+
+                            this.waitUntilVisible('#userOptionsLabel', function () {
+                                this.capture('login.png', undefined, {
+                                    format: 'png',
+                                    quality: 25
+                                });
+                            })
+                            // test.assertEquals(casper.popups.length, 0);
+                        });
+                    });
+                }
+    }).run(function() {
+        test.done();
+    });
+});
+//
 casper.test.begin('goes to indeed.com', 3, function(test) {
     casper.start(URI, function() {
 
@@ -11,6 +52,8 @@ casper.test.begin('goes to indeed.com', 3, function(test) {
         test.assertExists('#what');
         this.sendKeys('input#what', 'developer');
         this.sendKeys('input#where', 'Edmonton AB');
+
+        //var self = this;
 
         /* Click Search */
         this.click('input#fj');
@@ -20,12 +63,35 @@ casper.test.begin('goes to indeed.com', 3, function(test) {
         this.waitUntilVisible('div.row.result', function () {
             test.pass('Showing jobs');
             var data = this.getElementsInfo('div.row.result');
+
+            // this.capture('post.png', undefined, {
+            //     format: 'png',
+            //     quality: 25
+            // });
+
             _.each(data, function (element, index, list) {
                 casper.page.content = list[index]['html'];
-                if (casper.exists('span.iaLabel')) {
+
+                var didApply = false;
+
+                var appliedLabel = casper.exists('a.myjobs-serp-link');
+
+                if (appliedLabel) {
+                    var applied = casper.getElementInfo('a.myjobs-serp-link');
+                    applied = applied['text'].trim("");
+                    if (applied === 'Applied') didApply = true;
+                }
+
+                var easilyLabel = casper.exists('span.iaLabel');
+
+                if (easilyLabel && !didApply)  {
+
                         var easily = casper.getElementInfo('span.iaLabel')
                         easily = easily['text'].trim("");
-                        if (easily === EASILY) {
+
+                        /* FIXME not always the case. */
+
+                        if (easily === EASILY || easily === EASILY2) {
                             var obj = casper.getElementInfo('a.turnstileLink');
                             var url = URI + obj['attributes']['href'];
                             var title = obj['attributes']['title'];
@@ -44,6 +110,20 @@ casper.test.begin('goes to indeed.com', 3, function(test) {
 
                 }
             });
+
+            // this.waitUntilVisible('a.myjobs-serp-link', function () {
+            //     // this.capture('results.png', undefined, {
+            //     //     format: 'png',
+            //     //     quality: 25
+            //     // });
+            //     var applied = this.getElementInfo('a.myjobs-serp-link')
+            //     applied = applied['text'];
+            //     //utils.dump();
+            //     this.echo(applied);
+            // });
+            //this.page.content = self.page.content;
+
+
         });
         test.assertTitleMatch(/Job Search/, 'indeed.com has the right title');
     }).run(function() {
